52c7edc88a9553bebcc592f55c86dff34c65725b
Sam Khawase <sam.khawse@gmail.com>
2019-03-29 10:38:54 +0100

    Added Photos model and the unit test for it

--- WalkaboutTests/WalkaboutTests.swift
+++ WalkaboutTests/WalkaboutTests.swift
@@ -7,27 +7,48 @@
 //
 
 import XCTest
-@testable import Walkabout
+import Quick
+import Nimble
 
-class WalkaboutTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class WalkaboutTests: QuickSpec {
+    private let jsonData = """
+                    {
+                      "photos": {
+                        "page": 1,
+                        "pages": "733078",
+                        "perpage": 1,
+                        "photo": [
+                          {
+                            "farm": 8,
+                            "id": "33596152888",
+                            "isfamily": 0,
+                            "isfriend": 0,
+                            "ispublic": 1,
+                            "owner": "48708043@N00",
+                            "secret": "dd3a574ddb",
+                            "server": "7819",
+                            "title": "Blue In The Face"
+                          }
+                        ],
+                        "total": "733078"
+                      },
+                      "stat": "ok"
+                    }
+                   """.data(using: .utf8)
+    override func spec() {
+        context("Test for JSON Parsing") {
+            describe("This group tests the model parsing", closure: {
+                it("should parse the data correctly", closure: {
+                    let targetUrl = "https://farm8.staticflickr.com/7819/33596152888_dd3a574ddb_c.jpg"
+                    if let jData = self.jsonData,
+                        let response = try? JSONDecoder().decode(Response.self, from:jData) {
+                        expect(response).toNot(beNil())
+                        expect(response.photos.first?.photoUrl).to(equal(targetUrl))
+                    } else {
+                        fail("photo can't be parsed")
+                    }
+                })
+            })
         }
     }
 
